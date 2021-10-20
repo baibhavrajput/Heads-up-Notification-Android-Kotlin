5505c77c921dd344eb4d9dc4c1b41ecb2899dced
baibhavrajput <baibhavrajputt@gmail.com>
2021-10-20 22:07:50 +0530

    initial commit

---
+++ app/src/main/java/com/example/headsupnotification/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.headsupnotification
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = (NotificationChannel("first" , "default" , NotificationManager.IMPORTANCE_HIGH))
+
+            channel.apply { enableLights(true)
+                            enableVibration(true)}
+
+            nm.createNotificationChannel(channel)
+        }
+
+        button.setOnClickListener{
+            val builder =
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    Notification.Builder(this, "first")
+                }
+                else{
+                    Notification.Builder(this)
+                        .setPriority(Notification.PRIORITY_HIGH)
+                        .setDefaults(Notification.DEFAULT_VIBRATE or Notification.DEFAULT_LIGHTS)
+                }
+
+            val simpleNotification = builder
+                .setContentTitle("Heads up notification")
+                .setContentText("This is the sample description of the notification")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .build()
+
+            nm.notify(1,simpleNotification)
+        }
+
+
+    }
+}
